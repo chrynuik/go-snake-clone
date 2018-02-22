b51d8d2b10930984c77df166595d102e0210acda
Graeme Scott <graeme@pixelunion.net>
2018-02-21 17:39:32 -0800

    Add graph functions

--- graph.go
+++ graph.go
@@ -23,16 +23,47 @@ func (g *Graph) create(width int, height int) {
 	}
 }
 
-func (g Graph) isTileAccessible(tile Tile) bool {
+func (g *Graph) isTileAccessible(tile Tile) bool {
 	// does it exist? on the board?
+	if tile.X < 0 || tile.X >= g.Width {
+		return false
+	} else if tile.Y < 0 || tile.Y >= g.Height {
+		return false
+	} else {
+		return true
+	}
 }
 
 func (g Graph) neighbors(tile Tile) []Tile {
 	// iterate over neighbors, is accesssible? return array of accessible neighbors
+	directions := []Tile{}
+	directions[0] = Tile{X: 1, Y: 0}
+	directions[1] = Tile{X: 0, Y: 1}
+	directions[2] = Tile{X: -1, Y: 0}
+	directions[3] = Tile{X: 0, Y: -1}
+
+	results := []Tile{}
+
+	for _, direction := range directions {
+		neighbor := Tile{X: tile.X + direction.X, Y: tile.Y + direction.Y}
+
+		if g.isTileAccessible(neighbor) {
+			results = append(results, neighbor)
+		}
+	}
+
+	return results
 }
 
-func (g Graph) cost(tile Tile, direction Tile) {
+func (g *Graph) cost(tile Tile, direction Tile) int {
 	// the value in the tile
+	targetNode := Tile{X: tile.X + direction.X, Y: tile.Y + direction.Y}
+
+	if g.isTileAccessible(targetNode) {
+		return g.Grid[tile.Y][tile.X]
+	}
+
+	return 999
 }
 
 func (g Graph) String() string {
